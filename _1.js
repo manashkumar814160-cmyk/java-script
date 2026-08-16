9cd937b550a7eb83fa39c2372ed01c634346c245
Manash kumar <manashkumar814160@gmail.com>
2026-08-16 13:01:19 +0530

    first push

---
+++ _1.js
@@ -0,0 +1,19 @@
+// Change heading
+document.getElementById("heading").innerText = "Welcome to My Website";
+
+// Change paragraph
+document.getElementById("message").innerText =
+    "JavaScript is working!";
+
+// Change background
+document.body.style.backgroundColor = "lightblue";
+
+// Button function
+function changeColor() {
+    document.body.style.backgroundColor = "lightgreen";
+}
+
+// Show message
+function showMessage() {
+    alert("Hello! Welcome to my website.");
+}
